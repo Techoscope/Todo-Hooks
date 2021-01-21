36b88f11aedc89acb9bd71912fa4540604224862
Cemil Okay <cemilokay@gmail.com>
2021-01-21 03:25:33 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <div style={{marginLeft: 30}}>
+      <h1>Todo Hooks</h1>
+      <form>
+        <input type="text"/>
+      </form>
+      <ul>
+        <li>It works</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
